e4ada9f5236825b5c7715ee919568b587d447dd3
Your Name <you@example.com>
2022-03-29 01:09:55 +0700

    [phongvq] models database

--- src/models/Conversation.js
+++ src/models/Conversation.js
@@ -1,7 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const conversationSchema = new mongoose.Schema({
+    title: {
+        type:String,
+        required: true,
+    },
 
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+
+    modifiedDate: {
+        type: Date,
+        default: Date.now,
+    },
+
+    members: {
+        type: Array,
+    },
 });
 
 module.exports = mongoose.model('Conversation', conversationSchema);
--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,71 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true,
+    },
 
+    email: {
+        type: String,
+        unique: true,
+    },
+    
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 60,
+    },
+    
+    firstName: {
+        type: String,
+    },
+    
+    lastName: {
+        type: String,
+    },
+    
+    fullName: {
+        type: String,
+        required: true,
+    },
+    
+    // male -> 0
+    // female -> 1
+    // others -> 2
+    gender: {
+        type: Number,
+        required: true,
+    },
+
+    dateOfBirth: {
+        type: Date,
+        required: true,
+    },
+
+    address: {
+        type: String,
+    },
+
+    // offline -> 0
+    // online -> 1
+    // notWorking -> 2
+    userStatus: {
+        type: Number,
+        default: 0
+    },
+
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+
+    modifiedDate: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = mongoose.model('User', userSchema);
